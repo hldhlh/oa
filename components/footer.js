2309239275e7f8fa954fe0db11848299d42b012f
狐狸的狐狸画 <740740048@qq.com>
2025-07-05 01:55:25 +0800

    重构

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-export function getFooter() {
-    return `
-        <footer class="bg-black text-white py-4 mt-8">
-            <div class="container mx-auto px-6 text-center">
-                <p>&copy; ${new Date().getFullYear()} OA 系统. 保留所有权利.</p>
-            </div>
-        </footer>
-    `;
-} 
